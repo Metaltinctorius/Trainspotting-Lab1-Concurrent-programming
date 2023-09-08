d977df22eb4e610f12b2f16c7d23b069d78a190d
nikand <niklas.andersson91@hotmail.com>
2023-09-08 10:12:38 +0200

    Train can now run forward and backwards

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,36 +1,112 @@
-import TSim.*;
+import java.util.ArrayList;
+
+        import TSim.*;
 
 public class Lab1 {
 
   public Lab1(int speed1, int speed2) {
+
+    // Communication between simulator and program. (channel).
     TSimInterface tsi = TSimInterface.getInstance();
-    // Need to create individual sensor objects to be able to separate different events?
-    //SensorEvent sensor1 = new SensorEvent(1, 10, 7, 0);
-    //System.out.println(sensor1);
+
+    // Train objects.
+    Train train1 = new Train(1, speed1, Train.Direction.SOUTH);
+//    Train train2 = new Train(2, speed2, Train.Direction.NORTH);
+
+    // Threads for the trains (input: train)
+    Thread trainThread1 = new Thread(train1);
+   // Thread trainThread2 = new Thread(train2);
+
+    // Start the threads
+    trainThread1.start();
+    //trainThread2.start();
+
     try {
 
+     // Default params for program initialization.
       tsi.setSpeed(1, speed1);
-      if (tsi.getSensor(1).getStatus() == 1) {
-        tsi.setSpeed(1, 0);
-      }
-      //tsi.getSensor(1);
-
-      tsi.setSwitch(15,9, TSimInterface.SWITCH_RIGHT) ;
-
-      //tsi.setSpeed(2,0);
-      //sensorX = tsi.getSensor(2);
-
-
-      // tsi.getSensor(2) = sensor1;
-
+ //     tsi.setSpeed(2, speed2);
 
     } catch (CommandException e) {
-      e.printStackTrace();// or only e.getMessage() for the error
-      System.err.println(e.getMessage());
+      e.printStackTrace(); // or only e.getMessage() for the error
       System.exit(1);
-    } catch (InterruptedException e) {
-      throw new RuntimeException(e);
+
     }
   }
-}
 
+  class Train implements Runnable {
+
+    // Creation of a tsi interface
+    TSimInterface tsi = TSimInterface.getInstance();
+
+    // Parameters for a train
+    int id;
+    int speed;
+    Direction direction;
+
+    // Constructor for a train
+    public Train(int id, int speed, Direction direction) {
+      this.id = id;
+      this.speed = speed;
+      this.direction = direction;
+    }
+
+    // Directions a train can go, either north or south (more develop to come)
+    enum Direction {
+      NORTH,
+      SOUTH
+    }
+    public void break_and_reverse(int id, int speed, SensorEvent se){
+      try {
+        tsi.setSpeed(id, 0);
+        Thread.sleep(1000);
+        System.out.println("Wait OK");
+        tsi.setSpeed(id, -speed);
+      } catch (Exception e) {
+        e.printStackTrace();
+      }
+    }
+    @Override
+    public void run() {
+      try {
+
+        while (true) {
+          SensorEvent sensorEvent = tsi.getSensor(id);
+
+          if (sensorEvent.getXpos() == 10 && sensorEvent.getYpos() == 7
+                  && sensorEvent.getStatus() == sensorEvent.ACTIVE) {
+            System.out.println("HELLO");
+          }
+          if (sensorEvent.getXpos() == 17 && sensorEvent.getYpos() == 9
+                  && sensorEvent.getStatus() == sensorEvent.ACTIVE) {
+            System.out.println("WORLD");
+            tsi.setSwitch(15, 9, TSimInterface.SWITCH_RIGHT);
+          }
+          if (sensorEvent.getXpos() == 16 && sensorEvent.getYpos() == 7
+                  && sensorEvent.getStatus() == sensorEvent.ACTIVE) {
+            tsi.setSwitch(17, 7, TSimInterface.SWITCH_RIGHT);
+          }
+          if (sensorEvent.getXpos() == 5 && sensorEvent.getYpos() == 9
+                  && sensorEvent.getStatus() == sensorEvent.ACTIVE) {
+            tsi.setSwitch(4, 9, TSimInterface.SWITCH_LEFT);
+          }
+          if (sensorEvent.getXpos() == 2 && sensorEvent.getYpos() == 11
+                  && sensorEvent.getStatus() == sensorEvent.ACTIVE) {
+            tsi.setSwitch(3,11, TSimInterface.SWITCH_RIGHT);
+          }
+          if (sensorEvent.getXpos() == 10 && sensorEvent.getYpos() == 13
+                  && sensorEvent.getStatus() == sensorEvent.ACTIVE) {
+          break_and_reverse(id, speed, sensorEvent);
+          }
+        }
+
+      } catch (Exception e) {
+        e.printStackTrace();
+        ;
+      }
+
+    }
+
+  }
+
+}
